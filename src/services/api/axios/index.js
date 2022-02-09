5308d4c76b0e73cec638e96eb873bd6ad8a0870d
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-02-09 12:29:38 +0300

    Move  the base URL to the .env file

--- src/services/api/axios/index.js
+++ src/services/api/axios/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 axiosInstance.interceptors.request.use(
