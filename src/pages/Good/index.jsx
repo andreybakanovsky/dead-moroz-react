4b1f7437be37586f75a9877afc3b38692edcc2ef
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-02-13 00:00:00 +0300

    Create the Good page

---
+++ src/pages/Good/index.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import {
+  Container,
+} from "@mui/material";
+
+import api from "../../services/api";
+import {
+  useParams,
+  useNavigate
+} from 'react-router-dom';
+
+function Good() {
+  const id = useParams();
+  const navigate = useNavigate();
+  const [data, setData] = useState();
+
+  const loadData = useCallback(async () => {
+    try {
+      const response = await api.auth.getGood(id);
+      setData(response.data);
+    } catch (e) {
+      console.log(e.response.status);
+      console.log(e.response.data);
+      if (e.response.status === 404) {
+        navigate("/not-found-404");
+      }
+    }
+  }, [id, navigate]);
+
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
+
+  return (
+    <Container >
+      {data &&
+        <p>{data.id} | {data.year} | {data.content}</p>
+      }
+    </Container>
+  );
+}
+
+export default Good;
