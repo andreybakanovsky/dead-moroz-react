3a9df00b5365e2beb0f58ed915efef9c556a6c38
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-03-18 21:44:47 +0300

    Update My Good page
    Add a button 'addGift', update good's cards

--- src/pages/Goods/index.jsx
+++ src/pages/Goods/index.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState, useCallback } from 'react';
 import {
   Container,
   Card,
-  CardActionArea,
+  CardHeader,
   CardMedia,
   CardContent,
+  CardActions,
   Typography,
-  Grid
+  Grid,
+  IconButton
 } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
 
 import api from "../../services/api";
 import {
@@ -15,6 +19,7 @@ import {
   useNavigate,
   Link,
 } from 'react-router-dom';
+import '../../styles/App.css';
 
 function Goods() {
   let id = useParams();
@@ -57,27 +62,45 @@ function Goods() {
       >
         {data && data.map(good => {
           return <Grid item xs={12} sm={6} md={4} key={good.id}>
-            <Card sx={{ borderRadius: 3, m: 3, maxWidth: 345 }} >
-              <CardActionArea component={Link} to={`/users/${id.user_id}/goods/${good.id}`}>
-                {(good.images[0] !== undefined) ?
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    src={good.images[0].url}
-                    alt="good ..."
+            <Card sx={{ borderRadius: 2, m: 2, maxWidth: 345 }} >
+              <CardHeader
+                title={good.year}
+                action={
+                  <IconButton aria-label="edit"
+                    component={Link} to={`/users/${id.user_id}/goods/${good.id}`}
                   >
-                  </CardMedia>
-                  : null
+                    <EditIcon
+                      sx={{ color: 'darkgray', fontSize: 20 }}
+                    />
+                  </IconButton>
                 }
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {good.year}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {good.content}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
+              >
+              </CardHeader>
+              {(good.images[0] !== undefined) ?
+                <CardMedia
+                  component="img"
+                  height="140"
+                  src={good.images[0].url}
+                  alt="good ..."
+                >
+                </CardMedia>
+                : null
+              }
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  {good.content}
+                </Typography>
+              </CardContent>
+              <CardActions> 
+              <IconButton aria-label="gift"
+              component={Link} to={`/users/${id.user_id}/goods/${good.id}/gifts`}
+              >
+                <CardGiftcardIcon
+                  className="dead-moroz-red-color"
+                  sx={{ fontSize: 26 }}
+                />
+              </IconButton>
+              </CardActions>
             </Card>
           </Grid>
         })}
