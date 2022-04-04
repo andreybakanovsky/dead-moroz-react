fe22b75c9daed1b9aae5e235bee58a68fe7418f2
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-04-05 00:40:15 +0300

    Update endpoints

--- src/services/api/endpoints/auth.js
+++ src/services/api/endpoints/auth.js
@@ -25,7 +25,8 @@ const endpoints = {
   updateGift: (id, data) => axios.patch(`/v1/users/${id.user_id}/goods/${id.good_id}/gifts/${id.id}`, data),
   deleteRequestedGifts: (id) => axios.delete(`/v1/users/${id.user_id}/goods/${id.good_id}/gifts/${id.id}`),
 
-  getRequestedGift: (id) => axios.get(`/v1/users/${id.user_id}/goods/${id.good_id}/gifts/${id.id}`),
+  addSuggestedGift: (id, data) => axios.post(`/v1/users/${id.user_id}/goods/${id.good_id}/reviews/${id.review_id}/gifts`, data),
+
 
   getReviews: (id) => axios.get(`/v1/users/${id.user_id}/goods/${id.good_id}/reviews`),
   getReview: (id) => axios.get(`/v1/users/${id.user_id}/goods/${id.good_id}/reviews/${id.id}`),
