9b80251531fa352044e3793195c867278a891dc2
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-06-02 23:59:15 +0300

    Define access to the search page

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -13,7 +13,9 @@ import SearchIcon from '@mui/icons-material/Search';
 import useAuth from "../../hooks/useAuth";
 import api from "../../services/api";
 import parse, { domToReact } from 'html-react-parser';
-import { array } from 'yup';
+import {
+  Navigate,
+} from 'react-router-dom';
 
 
 function SearchPage() {
@@ -80,21 +82,20 @@ function SearchPage() {
   }
 
   return (
-    <Container >
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Paper
-          component="form"
-          sx={{ m: 2, p: '2px 4px', display: 'flex', alignItems: 'center', width: 600 }}
+    ((auth.user.role === "dead_moroz") || (auth.user.role === "elf")) ?
+      <Container >
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
         >
-          {(auth.user.role === "dead_moroz") &&
+          <Paper
+            component="form"
+            sx={{ m: 2, p: '2px 4px', display: 'flex', alignItems: 'center', width: 600 }}
+          >
             <InputBase
               onKeyPress={(e) => {
-                console.log(`Pressed keyCode ${e.key}`);
                 if (e.key === 'Enter') {
                   search();
                   e.preventDefault();
@@ -105,41 +106,44 @@ function SearchPage() {
               inputProps={{ 'aria-label': 'search gifts' }}
               inputRef={searchRef}
               onChange={e => setQueryString(e.target.value)}
-            />}
-          <IconButton
-            onClick={() => search()}
-            sx={{ p: '10px' }}
-            aria-label="search">
-            <SearchIcon />
-          </IconButton>
-        </Paper>
-      </Grid>
-      {results && (results.gifts != undefined) && results.gifts.map((gift) => (
-        <Paper
-          key={gift._id}
-          sx={{
-            m: 4,
-            p: 2,
-            margin: 1,
-            maxWidth: 'auto',
-            flexGrow: 1
-          }}
-        >
-          <Typography
-            className="dead-moroz-red-color"
-            variant="h6"
-            gutterBottom
+            />
+            <IconButton
+              onClick={() => search()}
+              sx={{ p: '10px' }}
+              aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+
+        </Grid>
+        {results && (results.gifts != undefined) && results.gifts.map((gift) => (
+          <Paper
+            key={gift._id}
+            sx={{
+              m: 4,
+              p: 2,
+              margin: 1,
+              maxWidth: 'auto',
+              flexGrow: 1
+            }}
           >
-            {capitalizeFirstLetter(gift._index)}
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            {getDate(gift._source.updated_at)} - {getHighlightedText(gift)}
-          </Typography>
-          <Typography variant="body2" gutterBottom>
-          </Typography>
-        </Paper>
-      ))}
-    </Container>
+            <Typography
+              className="dead-moroz-red-color"
+              variant="h6"
+              gutterBottom
+            >
+              {capitalizeFirstLetter(gift._index)}
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              {getDate(gift._source.updated_at)} - {getHighlightedText(gift)}
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+            </Typography>
+          </Paper>
+        ))}
+      </Container>
+      :
+      <Navigate to={"/not-found-404"} />
   );
 }
 
