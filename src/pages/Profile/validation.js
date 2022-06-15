9b7e5f5056b2c7043a5a164ae8b203e0766ac0d3
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-06-15 22:14:30 +0300

    Update all yup numbers validations
    Fix default NaN -messages to custom

--- src/pages/Profile/validation.js
+++ src/pages/Profile/validation.js
@@ -2,7 +2,12 @@ import * as yup from "yup";
 
 const schema = yup.object().shape({
   name: yup.string().min(2).max(20).required(),
-  age: yup.number().required().positive().integer(),
+  age: yup
+    .number()
+    .typeError('age must be a number')
+    .required()
+    .positive()
+    .integer(),
 });
 
 export default schema;
